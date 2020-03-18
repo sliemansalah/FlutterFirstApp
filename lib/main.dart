b041be50a1d68b9ffc971ad56db34ee9dcc8594c
slieman <slieman2001@hotmail.com>
2020-03-19 01:23:18 +0200

    switch case

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... if else
+  // learn dart ... switch
 
-  var x = 10;
+  var command = 'OPEN';
 
-  if(x > 5) {
-    print("x>5");
-  }else if (x== 5){
-    print("x=5");
-  }else{
-    print("x<5");
+  switch(command){
+    case  'CLOSED':
+    excuateClosed();
+    break;
+    case 'PENDING':
+    excuatePending();
+    break;
+    case 'open':
+    excuateOpen();
+    break;
+    default:
+    excuateUnKnown();
   }
 
-  bool isOk = false;
-
-  if(isOk){
-    print("ok");
-  }else{
-    print("not ok");
-  }
-
-   if(!isOk){
-    print("not ok");
-  }else{
-    print("ok");
-  }
-
-  
 }
