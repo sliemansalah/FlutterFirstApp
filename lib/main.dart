654bb0b4a74312cdcf673cc0fde2863ddea05440
slieman <slieman2001@hotmail.com>
2020-03-19 01:20:37 +0200

    if else..

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ...
-  
-  // $ not used on string as we used on variables. 
-  /* 
-  you can use as here
-  */
+  // learn dart ... if else
 
-  String s = "Ahmad\$s1\n";
-  
-  s+="test";
+  var x = 10;
 
-  print(s.substring(0,10));
-  print(s.replaceAll("Ah", "cc"));
+  if(x > 5) {
+    print("x>5");
+  }else if (x== 5){
+    print("x=5");
+  }else{
+    print("x<5");
+  }
 
-  var h = s.contains("h")?true:false;
-  print(h);
+  bool isOk = false;
 
-  String list = "ahmad, mohmed, salah";
-  List<String> names = list.split(","); // to convert list from string to array
-  names.forEach((v){
-    print(v);
-  });
+  if(isOk){
+    print("ok");
+  }else{
+    print("not ok");
+  }
 
-String list2 = "ahmad mohmed salah";
-  List<String> names2 = list.split(" "); // to convert list from string to array
-  names2.forEach((v){
-    print(v);
-  });
-
-  list2.trim(); // remove spaces
-  list2.trimLeft(); // remove spaces left
-  list2.trimRight(); // remove spaces right
-
-  print(list2.toUpperCase());
-  print(list2.toLowerCase());
-
-
-
-
-
+   if(!isOk){
+    print("not ok");
+  }else{
+    print("ok");
+  }
 
   
 }
