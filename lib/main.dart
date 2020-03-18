63aff5e07c5fe1254d4fe437f4e994429d3d3301
slieman <slieman2001@hotmail.com>
2020-03-19 01:02:08 +0200

    first app...

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child:
+    Text(
+      "Hello Flutter By Solayman M. Salah",
+       textDirection:TextDirection.ltr) // Text
+    ) // Center
+  );
+}
