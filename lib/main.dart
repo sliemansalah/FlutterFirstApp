02b9663f6f49d6ee2d533f59343e8f0f9670c214
slieman <slieman2001@hotmail.com>
2020-03-19 01:09:46 +0200

    vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child:
-    Text(
-      "Hello Flutter By Solayman M. Salah",
-       textDirection:TextDirection.ltr) // Text
-    ) // Center
-  );
+  // learn dart ...
+  print("Hello World"); // just print on console
+  var world = "Hello World"; // declare variable
+  world= 'Hi Ahmed'; //reassign value
+  print(world);
+  
+  const name = "Ahmed"; // declare const
+  print(name);
+
+  var number  = 10; 
+  // num age = "test"; // this is number
+  num age2= 9;
+  // String s = 9; // this is a string
+  String s = "ss";
+  age2+=6;
+  print("age is " + age2.toString());
+  print("age is $age2");
+
+  int a = 10;
+  double b = 10.5;
+  bool t = true;
+  
+  
 }
