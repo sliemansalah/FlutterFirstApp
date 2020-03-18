45bec1d75cb3b7c941e536a26172fd69b76a7d7c
slieman <slieman2001@hotmail.com>
2020-03-19 01:17:25 +0200

    string functions ..

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   // learn dart ...
-  print("Hello World"); // just print on console
-  var world = "Hello World"; // declare variable
-  world= 'Hi Ahmed'; //reassign value
-  print(world);
   
-  const name = "Ahmed"; // declare const
-  print(name);
+  // $ not used on string as we used on variables. 
+  /* 
+  you can use as here
+  */
 
-  var number  = 10; 
-  // num age = "test"; // this is number
-  num age2= 9;
-  // String s = 9; // this is a string
-  String s = "ss";
-  age2+=6;
-  print("age is " + age2.toString());
-  print("age is $age2");
-
-  int a = 10;
-  double b = 10.5;
-  bool t = true;
+  String s = "Ahmad\$s1\n";
   
+  s+="test";
+
+  print(s.substring(0,10));
+  print(s.replaceAll("Ah", "cc"));
+
+  var h = s.contains("h")?true:false;
+  print(h);
+
+  String list = "ahmad, mohmed, salah";
+  List<String> names = list.split(","); // to convert list from string to array
+  names.forEach((v){
+    print(v);
+  });
+
+String list2 = "ahmad mohmed salah";
+  List<String> names2 = list.split(" "); // to convert list from string to array
+  names2.forEach((v){
+    print(v);
+  });
+
+  list2.trim(); // remove spaces
+  list2.trimLeft(); // remove spaces left
+  list2.trimRight(); // remove spaces right
+
+  print(list2.toUpperCase());
+  print(list2.toLowerCase());
+
+
+
+
+
+
   
 }
