d7d2985b7bfd15673431da234057a99df0ea7980
slieman <slieman2001@hotmail.com>
2020-03-19 01:29:11 +0200

    for loop in array,default,list

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... while loop
+  // learn dart ... for loop
 
-  bool running = true;
-  int count = 0;
-  while(running){
-    print("Hello World $count");
-    if(count == 5){
-      running= false;
-    }else{
-      count++;
-    }
+  var arr= [1,2,3,4,5];
+
+  for(int i=0;i<=5;i++){
+    print("count $i");
   }
+  for(int i=0;i<=arr.length - 1;i++){
+    print("array element " + arr[i].toString());
+  }
+
+  List<String> arrlist = new List<String>();
+  arrlist.add("ahmad");
+  arrlist.add("mohammed");
+  arrlist.add("salah");
+
+  arr.forEach((str){
+    print("List element " + str.toString());
+  });
 }
