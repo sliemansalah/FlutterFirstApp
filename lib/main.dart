7381cf36582723ff3bc75cbaf79f623e68221806
slieman <slieman2001@hotmail.com>
2020-03-19 02:28:47 +0200

    static vars and methods.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... Polymarphism
+  // learn dart ... static vars and methods
+  // var animal = Animal();
+  // animal.color="Black";
+  // animal.walk();
+  // var animal2= Animal();
 
-  var dog = Dog();
-  dog.walk();
-  dog.run();
-  dog.eat();
+  // Animal.color = "Black";
+  Animal.walk();
+
+  // Animal.color = null;
+  // static reserved fixed on memory.
 
 }
 
-class Carnivores {
-  void eat() {
-    print("animal is eating");
-  }
-}
-abstract class Animal { // abstract class, not create instance
-  void walk();
-  void run() {
-    print("animal is running");
+class Animal {
+  static const String color = "Black";
+
+  static void walk(){
+    print("Animal is walking");
   }
 }
 
-// class Dog extends Animal, Carnivores{ can't make multible inhritance
-class Dog implements Carnivores, Animal{
-  // class Dog extends Animal implements Carnivores{
-  void walk(){
-    print("dog is walking");
-  }
-
-  void run(){
-    // super.run(); can't use as implement
-    print("dog is running");
-  }
-
-  // @override  // not must written
-  void eat() {
-    // super.run(); can't use as implement
-    // super.walk(); // can't be used.
-    print("dog is eating");
-  }
-}
