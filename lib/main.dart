cae57cf15896ab4f8bd9270864e8baaad1a1fc4f
slieman <slieman2001@hotmail.com>
2020-03-19 02:04:08 +0200

    Inheritance

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... Constructor
+  // learn dart ... Inheritance
+  var dog1 = Dog();
+  // dog1.color="Black";
+  dog1.length= 1;
+  dog1.walk();
+  dog1.run();
 
-  // var student1 = new Student();
-  // student1.stuNumber=1;
-  // student1.stuName="Mohammed";
-  // student1.study();
-
-  var student1 = new Student.newCons(1, "Ahmad");
-  student1.study();
-
-  var student2 = new Student.newCons(2, "Soso");
-  student2.study();
-
-  var student3 = new Student();
-  student3.stuNumber=3;
-  student3.stuName="dodo";
-  student3.study();
-
- // in dart (no overloading); but we can solve
+  var cat1 = Cat();
+  cat1.walk();
+  cat1.climb();
 
 }
 
+class Animal {
+  String color ="";
+  int length = 0;
 
-class Student {
-  // int stuNumber = 0;
-  // String stuName ="";
-int stuNumber;
-  String stuName;
-  Student(){ // this is constructor
-  print("new class");
-  this.stuNumber = 0;
-  this.stuName ="";
+  void walk(){
+    print("Animal is walking");
   }
-  //  Student(int num, String name){ // this is constructor
-  // this.stuNumber = 0;
-  // this.stuName ="";
+}
 
-   Student.newCons(int val, [String name,int num]){ // this is constructor [optional params]
-  this.stuNumber = 0;
-  this.stuName ="";
+class Dog extends Animal {
+  String color ="Black";
+   void walk() {
+     super.walk();
+   print("dog is walking"); //shadowing.. 
   }
-
-   Student.newCons2(int val, String name){ // this is constructor [optional params]
-  this.stuNumber = 0;
-  this.stuName ="";
+  void run() {
+    print("dog is running");
   }
+}
 
-  Student.newCons3(this.stuNumber, this.stuName); // this is amazing constructor.
-  void study(){
-    print("student $stuName with number $stuNumber is studing");
+class Cat extends Animal {
+  void climb(){
+    print("cat is climbing");
   }
 }
