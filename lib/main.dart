5572813d3c5e595ef23f1efdac3ba75f2e803cb4
slieman <slieman2001@hotmail.com>
2020-03-19 02:13:23 +0200

    Constructor in Inheritance

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... Inheritance
-  var dog1 = Dog();
-  // dog1.color="Black";
-  dog1.length= 1;
-  dog1.walk();
-  dog1.run();
-
-  var cat1 = Cat();
-  cat1.walk();
-  cat1.climb();
+  // learn dart ... Constructor in Inheritance
+  // var dog1 = Dog();
+  // var dog1 = Dog(1);
+    var dog1 = Dog(1,"Black");
 
 }
 
 class Animal {
-  String color ="";
-  int length = 0;
+  String color;
 
-  void walk(){
-    print("Animal is walking");
+  Animal(String Color){
+    this.color = Color;
+    print("Animal Cons");
+  }
+
+  Animal.animalNamedCons(){
+    print("Animal Named Cons Class");
   }
 }
 
 class Dog extends Animal {
-  String color ="Black";
-   void walk() {
-     super.walk();
-   print("dog is walking"); //shadowing.. 
-  }
-  void run() {
-    print("dog is running");
-  }
+   int length;
+    // Dog(int len){ // as Dog : super() by default
+    //   this.length = len;
+    //   print("Dog Class Cons");
+    // }
+
+    //  Dog(int len) : super("Black"){
+    //   this.length = len;
+    //   print("Dog Class Cons $len m class cons ");
+
+    Dog(int len, String color) : super("Black"){
+      this.length = len;
+      print("Dog Class Cons $len m class cons ");
+    }
+
+    Dog.namedCons() :super("Blue") { // :super.animalNamedCons
+      print("Dog named Cons Class");
+    }
 }
 
-class Cat extends Animal {
-  void climb(){
-    print("cat is climbing");
-  }
-}
