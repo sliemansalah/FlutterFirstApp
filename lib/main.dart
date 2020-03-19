6f8fa3cbbfd74e2b97687c16e2df1b0e9acd206e
slieman <slieman2001@hotmail.com>
2020-03-19 03:00:15 +0200

    first app

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,14 @@
+/* Working with flutter widgets */
+
 import 'package:flutter/material.dart';
 
 void main() {
-  // learn dart ... Handling Exceptions
-
-  var myclass= myClass();
-  myclass(0);
-
+  runApp(
+    Center(
+      child: Text(
+        "Hello World",
+        textDirection: TextDirection.ltr,
+      )
+    )
+  );
 }
-
-class myClass {
-  void call(int n1){
-    // try {
-    //   int result = 12 ~/ n1;  // ~ to be res int
-    // print("the result is $result");
-    // } on IntegerDivisionByZeroException {
-    //   print("Do not div by 0");
-    // }catch(m, s){
-    //   print("the error is $m");
-    //   // print(s);
-    // }finally {
-    //   print("code must run");
-    // }
-
-     try {
-    deposit(n1);
-  } catch (m) {
-    print("the error = $m");
-  }
-  }
-
-  void deposit(int money){
-    if(money<0){
-      throw MyEx();
-    }
-  }
-}
-
-class MyEx implements Exception { 
-  String errorMessage (){
-    return "Don't do this message";
-  }
-}
-
-
